db2547c56ab9372bd0b2d63d934d8dff0890a1cf
Anthony Usypchuk <1482998@johnabbottcollege.net>
2018-11-14 00:04:18 -0500

    Added stupid button tabarnaq

--- app/src/main/java/com/usypchuk/andrewanthonyproject/MainActivityFragment.java
+++ app/src/main/java/com/usypchuk/andrewanthonyproject/MainActivityFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,25 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view1 = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button butt1; //Create button
+        butt1 = view1.findViewById(R.id.buttonx); //find the button in the display
+
+        butt1.setOnClickListener(new Button.OnClickListener() {   //set a listener to wait for it to be pressed
+            @Override
+            public void onClick(View v) {                       //when it is pressed do this stuff (View v is the button)
+
+                TextView text1 = getActivity().findViewById(R.id.text1);
+
+                if(text1.getVisibility() == View.INVISIBLE)
+                    text1.setVisibility(View.VISIBLE);
+                else
+                    text1.setVisibility(View.INVISIBLE);
+            }
+        });
+
+        return view1;
+
     }
 }
